90f80fc0393669510eb49f21b6e9412a6b1e8d4b
AndyIndemans <andy.indemans@outlook.com>
2022-03-24 15:32:34 +0100

    Implemented logging for troubleshooting (control inputs)

--- micromobility_car_pilot/local/Project/dev_ws/src/pilot/pilot/pilot_node_without_oa.py
+++ micromobility_car_pilot/local/Project/dev_ws/src/pilot/pilot/pilot_node_without_oa.py
@@ -5,12 +5,30 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 from std_msgs.msg import Bool
 from mm_interfaces.msg import CarInstructions
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger(__name__)
 
 
 class PilotNodeWithoutOa(Node):
 
     def __init__(self):
         super().__init__('pilot_node')
+
+        log_file = "../output/logs/" + "navigation.log"
+
+        log_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        rotating_file_handler = RotatingFileHandler(log_file, mode="w", maxBytes=1000000 * 10, backupCount=7)
+        rotating_file_handler.setFormatter(log_format)
+        rotating_file_handler.setLevel(logging.DEBUG)  # Levels: DEBUG < INFO < WARNING < ERROR
+
+        root_logger = logging.getLogger("")
+        root_logger.setLevel(logging.DEBUG)
+        root_logger.addHandler(rotating_file_handler)
+
+        logger.info("\nNEW RUN IN PROGRESS")
+
         self.subscription = self.create_subscription(Float64, 'next_heading', self.next_heading_callback, 10)
 
         self.car_instructions_publisher = self.create_publisher(CarInstructions, 'instructions', 10)
@@ -22,6 +40,7 @@ class PilotNodeWithoutOa(Node):
         self.throttle = int(os.environ['DEFAULT_THROTTLE'])
         self.next_heading = 0
         self.return_to_node_busy = False
+        logger.debug("Pilot running")
 
     def next_heading_callback(self, msg):
         self.get_logger().info('Next heading: "%s"' % msg.data)
@@ -35,6 +54,7 @@ class PilotNodeWithoutOa(Node):
         #self.get_logger().info("throttle: " + str(msg.throttle))
         #self.get_logger().info("in next heading")
         self.get_logger().info("to car, angle: " + str(msg.angle) + " and throttle: " + str(msg.throttle))
+        logger.debug("to car, angle: " + str(msg.angle) + " and throttle: " + str(msg.throttle))
         self.car_instructions_publisher.publish(msg)
         self.final_car_instructions_publisher.publish(msg)
         # wordt deze callback ondertussen uitgevoerd?
